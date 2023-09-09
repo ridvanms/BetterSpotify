6db63606d41ab656ec690286cf3fd7b78798eea9
Ridvan <ridvans@abv.bg>
2023-09-09 20:38:01 +0300

    Updated dependencies, added new features, refactored code

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import './index.css';
 import App from './App';
+import './index.css';
 import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,12 +1,34 @@
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '22fb187c8dmshc113255b748227ap1d6ff0jsn01412e1107b1',
-    'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com',
-  },
-};
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-fetch('https://genius-song-lyrics1.p.rapidapi.com/chart/songs/', options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': '22fb187c8dmshc113255b748227ap1d6ff0jsn01412e1107b1',
+//     'X-RapidAPI-Host': 'shazam-api7.p.rapidapi.com',
+//   },
+// };
+
+// fetch('https://shazam-api7.p.rapidapi.com/charts/get-top-songs-in-world', options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-api7.p.rapidapi.com/',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        '22fb187c8dmshc113255b748227ap1d6ff0jsn01412e1107b1',
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => 'charts/get-top-songs-in-world' }),
+  }),
+});
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
