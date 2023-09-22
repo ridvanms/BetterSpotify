2e2b87d6fc46a90532b15d89315f01bdd2dedee7
ridvanms <ridvans@abv.bg>
2023-09-22 19:02:20 +0300

    updating

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,17 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { RiCloseLine } from 'react-icons/ri';
+
+import { logo } from '../assets';
+import { links } from '../assets/constants';
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  return (
+    <div className="md:flex hidden flex-col w-[ 240px] py-10 px-4 bg-[#1f1f1f]">
+      <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+    </div>
+  );
+};
 
 export default Sidebar;
